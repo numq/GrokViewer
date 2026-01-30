9847b9bcbc2f49e5260c0f72330b226e4eebdaa6
numq <numq.dev@gmail.com>
2026-01-30 13:00:54 +0300

    Provide refactoring

--- desktop/src/main/kotlin/io/github/numq/grokviewer/window/WindowDecoration.kt
+++ desktop/src/main/kotlin/io/github/numq/grokviewer/window/WindowDecoration.kt
@@ -4,13 +4,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.window.WindowDraggableArea
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Fullscreen
 import androidx.compose.material.icons.filled.FullscreenExit
 import androidx.compose.material.icons.filled.Minimize
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
